341086dbb3d314654e05f96115af9ef2916a4388
Sam Sharff <samanthaleec@gmail.com>
2022-03-13 16:23:42 -0400

    adds working example of url segment params

--- app/controllers/params_controller.rb
+++ app/controllers/params_controller.rb
@@ -1,6 +1,13 @@
 class ParamsController < ApplicationController
   def all_caps
     input_phrase = params["user_input"].upcase
-    render json: input_phrase.upcase.as_json
+    render json: {message: "Here is your phrase in uppercase: #{input_phrase}"}.as_json 
+  end
+
+  def segment_params_caps
+    user_input_2 = params["butthead"].upcase
+    render json: {message: "Here we use url segment params to turn your phrase to all uppercase: #{user_input_2}."}.as_json
   end
 end
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  
   get "/all_caps", controller: "params", action: "all_caps"
+  get "/segment_params_caps/:butthead", controller: "params", action: "segment_params_caps"
 end
